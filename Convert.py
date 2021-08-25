082cfe088bfe62d8e80a1a304d5fad86b72748e4
mrrobi9947 <89423131+mrrobi9947@users.noreply.github.com>
2021-08-25 10:37:12 +0530

    Create Convert.py

---
+++ Convert.py
@@ -0,0 +1,17 @@
+import time
+
+var1 = ( 'hello here we can calculate kg')
+print(var1)
+
+time.sleep(3.1)
+
+weight_lbs = input('weight (lbs): ')
+time.sleep(4.1)
+
+weight_kg = int(weight_lbs) * 0.45
+print (weight_kg)
+
+time.sleep(4.1)
+
+var2 = ('thank you')
+print(var2)
